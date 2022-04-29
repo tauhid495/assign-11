077b448b61dbc334ccaee2407bd05e35213d650e
tauhid495 <96792668+tauhid495@users.noreply.github.com>
2022-04-29 11:19:51 +0600

    first commit

---
+++ src/Components/CommonRouts/NavBar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../header/caroImage/logo.png'
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav class="
+                relative
+                w-full
+                flex flex-wrap
+                items-center
+                justify-between
+                py-4
+                bg-gray-100
+                text-gray-500
+                hover:text-gray-700
+                focus:text-gray-700 
+                shadow-lg
+                navbar navbar-expand-lg navbar-light
+                ">
+                <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+
+                    <Link to='/'><img className='w-[130px]' src={logo} alt="" /></Link>
+
+                    <button class="
+                                navbar-toggler
+                                text-gray-500
+                                border-0
+                                hover:shadow-none hover:no-underline
+                                py-2
+                                px-2.5
+                                bg-transparent
+                                focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
+                            class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path fill="currentColor"
+                                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+                            </path>
+                        </svg>
+                    </button>
+                    <div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
+
+                        {/* <!-- Left links --> */}
+                        <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+
+                        </ul>
+                        {/* <!--start of Right links --> */}
+                        <ul class="navbar-nav flex flex-col pl-0 list-style-none ">
+                            <li as={Link} to='/login'  class="nav-item px-2">
+                                <a class="nav-link text-gray-500 hover:text-gray-700 p-0" aria-current="page" href='#'>Home</a>
+                            </li>
+                            <li class="nav-item pr-2">
+                                <a class="nav-link text-gray-500 hover:text-gray-700 p-0">Features</a>
+                            </li>
+                            <li class="nav-item pr-2">
+                                <Link to='/login' class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Login</Link>
+                            </li>
+                            <li class="nav-item pr-2">
+                               
+                            </li>
+                        </ul>
+
+                    </div>
+
+                    {/* <!-- Collapsible wrapper --> */}
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Components/header/Carousel.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import caro1 from'./caroImage/caro1.png'
+import caro2 from'./caroImage/caro2.png'
+import caro3 from'./caroImage/caro3.png'
+
+const Carousel = () => {
+    return (
+        <div>
+            <div
+                id="carouselDarkVariant"
+                className="carousel slide carousel-fade carousel-dark relative"
+                data-bs-ride="carousel"
+            >
+                {/* <!-- Indicators --> */}
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                    <button
+                        data-bs-target="#carouselDarkVariant"
+                        data-bs-slide-to="0"
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        data-bs-target="#carouselDarkVariant"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        data-bs-target="#carouselDarkVariant"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 1"
+                    ></button>
+                </div>
+
+                {/* <!-- Inner --> */}
+                <div className="carousel-inner relative w-full overflow-hidden">
+                    {/* <!-- Single item --> */}
+                    <div className="carousel-item active relative float-left w-full">
+                        <img
+                            src={caro1}
+                            className="block w-full"
+                            alt="Motorbike Smoke"
+                        />
+                        <div className="carousel-caption hidden md:block absolute text-start">
+                            <h5 className="text-xl">First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+
+                    {/* <!-- Single item --> */}
+                    <div className="carousel-item relative float-left w-full">
+                        <img
+                            src={caro2}
+                            className="block w-full"
+                            alt="Mountaintop"
+                        />
+                        <div className="carousel-caption hidden md:block absolute text-start">
+                            <h5 className="text-xl">Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+
+                    {/* <!-- Single item --> */}
+                    <div className="carousel-item relative float-left w-full">
+                        <img
+                            src={caro3}
+                            className="block w-full"
+                            alt="Woman Reading a Book"
+                        />
+                        <div className="carousel-caption hidden md:block absolute text-start">
+                            <h5 className="text-xl">Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                {/* <!-- Inner --> */}
+
+                {/* <!-- Controls --> */}
+                <button
+    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+    type="button"
+    data-bs-target="#carouselDarkVariant"
+    data-bs-slide="prev"
+  >
+    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button
+    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+    type="button"
+    data-bs-target="#carouselDarkVariant"
+    data-bs-slide="next"
+  >
+    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
