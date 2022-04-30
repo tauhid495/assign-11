f3db2a55b72778d7062c418c0184daac2d076e59
tauhid495 <96792668+tauhid495@users.noreply.github.com>
2022-05-01 01:02:56 +0600

    add product route updated

--- src/Components/CommonRouts/NavBar.js
+++ src/Components/CommonRouts/NavBar.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import logo from '../header/caroImage/logo.png'
 import CustomLink from './CustomLink';
 
 const NavBar = () => {
     return (
-        <div>
-            <nav class="
-                relative
-                w-full
-                flex flex-wrap
-                items-center
-                justify-between
-                py-4
-                bg-gray-100
-                text-gray-500
-                hover:text-gray-700
-                focus:text-gray-700 
-                shadow-lg
-                navbar navbar-expand-lg navbar-light
-                ">
-                <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <>
+            <nav className="
+             sticky sticky-top w-full flex flex-wrap items-center justify-between py-4 bg-gray-50 text-gray-500 hover:text-gray-700
+                shadow navbar navbar-expand-lg navbar-light ">
+                <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
 
                     <CustomLink to='/'><img className='w-[130px]' src={logo} alt="" /></CustomLink>
 
-                    <button class="
+                    <button className="
                                 navbar-toggler
                                 text-gray-500
                                 border-0
@@ -35,13 +23,13 @@ const NavBar = () => {
                                 focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
-                            class="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            className="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path fill="currentColor"
                                 d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
                             </path>
                         </svg>
                     </button>
-                    <div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
 
                         {/* <!-- Left links --> */}
                         <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
@@ -49,20 +37,20 @@ const NavBar = () => {
                         </ul>
                         {/* <!--start of Right links --> */}
                         <ul className="navbar-nav flex flex-col pl-0 list-style-none ">
-                            <li class="nav-item pr-2">
-                                <CustomLink to='/' class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Home</CustomLink>
+                            <li className="nav-item pr-2">
+                                <CustomLink to='/' className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Home</CustomLink>
                             </li>
-                            <li class="nav-item pr-2">
-                                <CustomLink to='/addproduct' class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Add Product</CustomLink>
+                            <li className="nav-item pr-2">
+                                <CustomLink to='/addproduct' className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Add Product</CustomLink>
                             </li>
-                            <li class="nav-item pr-2">
-                                <CustomLink to='/removeproduct' class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Remove Product</CustomLink>
+                            <li className="nav-item pr-2">
+                                <CustomLink to='/removeproduct' className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Remove Product</CustomLink>
                             </li>
-                            <li class="nav-item pr-2">
-                                <CustomLink to='/register' class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Register</CustomLink>
+                            <li className="nav-item pr-2">
+                                <CustomLink to='/register' className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Register</CustomLink>
                             </li>
-                            <li class="nav-item pr-2">
-                                <CustomLink to='/login' class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Login</CustomLink>
+                            <li className="nav-item pr-2">
+                                <CustomLink to='/login' className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" >Login</CustomLink>
                             </li>
                         </ul>
 
@@ -71,7 +59,7 @@ const NavBar = () => {
                     {/* <!-- Collapsible wrapper --> */}
                 </div>
             </nav>
-        </div>
+        </>
     );
 };
 
--- src/Components/ProductManagement/AddProduct.js
+++ src/Components/ProductManagement/AddProduct.js
@@ -1,62 +1,76 @@
 import React from 'react';
 
 const AddProduct = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className='md:flex md:flex-row h-screen items-center'>
             {/* image sec */}
             <div className='basis-1/2 p-4'>
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image"/>
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image" />
             </div>
 
             {/* form section */}
-            <div class="basis-1/2 mx-auto block p-4 rounded-lg shadow-lg bg-white max-w-sm">
-                <form>
+            <div className="basis-1/2 mx-auto block p-4 rounded-lg shadow-lg bg-white max-w-sm">
+                <form onSubmit={handleSubmit} className=''>
                     <div className='text-xl text-center'> Add Product</div>
-                    <div class="form-group mb-6">
-                        <label for="product name" class="form-label inline-block mb-2 text-gray-700">Product Name</label>
-                        <input type="text" class="form-control
+                    <div className="form-group mb-6">
+                        <label htmlFor="product name" className="form-label inline-block mb-2 text-gray-700">Product Name</label>
+                        <input type="text" className="form-control
                             block  w-full px-3 py-1.5 text-base font-normal
                             text-gray-700 bg-white bg-clip-padding
                             border border-solid border-gray-300 rounded
                             transition ease-in-out m-0
-                            " placeholder="Name"/>
+                            " placeholder="Name" />
                     </div>
 
-                    <div class="form-group mb-6">
-                        <label for="product image" class="form-label inline-block mb-2 text-gray-700">Product Image</label>
-                        <input type="text" class="form-control
+                    <div className="form-group mb-6">
+                        <label htmlFor="product image" className="form-label inline-block mb-2 text-gray-700">Product Image</label>
+                        <input type="text" className="form-control
                             block  w-full px-3 py-1.5 text-base font-normal
                             text-gray-700 bg-white bg-clip-padding
                             border border-solid border-gray-300 rounded
                             transition ease-in-out m-0
-                            " placeholder="Img URL"/>
+                            " placeholder="Img URL" />
                     </div>
-
-                    <div class="form-group mb-6">
-                        <label for="product Price" class="form-label inline-block mb-2 text-gray-700">Product Price</label>
-                        <input type="number" class="form-control
+                    <div className="form-group mb-6">
+                        <label htmlFor="product image" className="form-label inline-block mb-2 text-gray-700">Supplier Name</label>
+                        <input type="text" className="form-control
                             block  w-full px-3 py-1.5 text-base font-normal
                             text-gray-700 bg-white bg-clip-padding
                             border border-solid border-gray-300 rounded
                             transition ease-in-out m-0
-                            " placeholder="Price"/>
+                            " placeholder="Supplier" />
                     </div>
 
-                    <div class="form-group mb-6">
-                        <label for="product name" class="form-label inline-block mb-2 text-gray-700">Product Description</label>
-                        <textarea type="textarea" class="form-control
+                    <div className="form-group mb-6">
+                        <label for="product Price" className="form-label inline-block mb-2 text-gray-700">Product Price</label>
+                        <input type="number" className="form-control
                             block  w-full px-3 py-1.5 text-base font-normal
                             text-gray-700 bg-white bg-clip-padding
                             border border-solid border-gray-300 rounded
                             transition ease-in-out m-0
-                            " placeholder="Short Description"/>
+                            " placeholder="Price" />
                     </div>
-                    
 
-                    
-                    <button type="submit" class=" block mx-auto
+                    <div className="form-group mb-6">
+                        <label for="product name" className="form-label inline-block mb-2 text-gray-700">Product Description</label>
+                        <textarea type="textarea" className="form-control
+                            block  w-full px-3 py-1.5 text-base font-normal
+                            text-gray-700 bg-white bg-clip-padding
+                            border border-solid border-gray-300 rounded
+                            transition ease-in-out m-0
+                            " placeholder="Short Description" />
+                    </div>
+
+
+
+                    <button type="submit" className=" block mx-auto
                         px-6 py-2.5 bg-blue-600 text-white font-medium text-xs
-                        leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Submit</button>
+                        leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Click to Add</button>
                 </form>
             </div>
         </div>
